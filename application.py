6ae2ddc127811c4b83b72921b276e0a183f0ee0d
Kaindra Djoemena <kaindra.djoemena@gmail.com>
2022-01-04 17:14:48 +0700

    fixed database

--- application.py
+++ application.py
@@ -1,27 +1,59 @@
-from flask import Flask, redirect, render_template, request, session
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 import sqlite3
 import random
 
 app = Flask(__name__)
-conn = sqlite3.connect("database.db", check_same_thread=False)
-cursor = conn.cursor()
+
+# Initialize database
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+# Make tables
+class Teachers(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    sex = db.Column(db.String, nullable=False)
+    image = db.Column(db.String)
+    nip = db.Column(db.Integer)
+    nuptk = db.Column(db.Integer)
+    time = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"<id: {self.id}, name: {self.name}, sex: {self.sex}, image_path: {self.image}, nip: {self.nip}, nuptk: {self.nuptk}, time: {self.time}>"
+
+class Quotes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    teacher_id = db.Column(db.Integer, nullable=False)
+    quote = db.Column(db.String, nullable=False)
+    source = db.Column(db.String, nullable=False)
+    time = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"<id: {self.id}, teacher_id: {self.teacher_id}, quote: {self.quote}, source: {self.source}, time: {self.time}>"
+
 
 @app.route("/")
 def index():
-    fetched_data = cursor.execute("""SELECT * FROM quotes""").fetchall()
+    fetched_data = Quotes.query.all()
 
     # See if there is data
     if len(fetched_data) < 1:
         return render_template("error.html", message="database error")
 
     random_data = random.choice(fetched_data)
-    quote = random_data[2]
-    teacher_id = random_data[1]
+    quote = random_data.quote
+    teacher_id = random_data.teacher_id
 
-    teacher, image, sex = cursor.execute("""SELECT name, image, sex FROM teachers WHERE id=?""", (teacher_id,)).fetchall()[0]
-    if sex == "m":
-        name = "Pak " + teacher.capitalize()
-    elif sex == "f":
-        name = "Bu " + teacher.capitalize()
+    teacher_data = Teachers.query.filter_by(id=teacher_id)
 
-    return render_template("index.html", image=image, quote='"'+quote+'"', teacher=name)
+    teacher_data = Teachers.query.all()
+    if teacher_data.sex == "m":
+        name = "Pak " + teacher_data.name.capitalize()
+    if teacher_data.sex == "f":
+        name = "Bu " + teacher_data.name.capitalize()
+    
+
+    return render_template("index.html", image=teacher_data.image, quote='"'+quote+'"', teacher=name)
